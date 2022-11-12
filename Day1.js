e36f6d8119e2f6958e183bdadfe3ddc36c91e10a
Abid Habib ✅ <41414028+abidhabib@users.noreply.github.com>
2022-11-12 03:56:38 +0000

    Pending changes exported from your codespace

---
+++ Day1.js
@@ -0,0 +1,53 @@
+ // console.log(2+9);
+// const months=["Feb","March","April","May","Jun","July"]
+// months.push("Agust")
+// months.unshift("Jan")
+// months.shift()
+// console.log(months);
+
+
+
+//  const months=["Feb","March","April","May","Jun","July"]
+//  months.splice(months.length,5,"Dce")
+// console.log(months);
+
+
+// const months=["Feb","March","April","May","Jun","July"]
+// const updateMonth= months.indexOf("May")
+// if(updateMonth !==-1){
+//   const um=  months.splice(updateMonth,1,"may")
+// console.log(um);
+// }
+// else{
+//     console.log("not found")
+// }
+// console.log(months);
+
+
+
+// const months=["Feb","March","April","May","Jun","July"]
+// const updateMonth= months.indexOf("Jun")
+// if(updateMonth !==-1){
+//     months.splice(updateMonth,1)
+// }
+// else{
+//     console.log("not found")
+// }
+// console.log(months);
+
+
+
+
+//  const months=["Feb","March","April","May","Jun","July"]
+//  const AddMonth= months.splice(3,0,"Dce")
+//  console.log(AddMonth);
+//     console.log(months);
+
+
+//  const months=["Feb","March","April","May","Jun","July"]
+//  const mIndex=months.indexOf("May")
+//  const DeleteRest= months.splice(mIndex,Infinity)
+//  console.log(DeleteRest);
+//     console.log(months);
+
+// Day 2 //
